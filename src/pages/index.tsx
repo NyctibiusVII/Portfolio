1eab439efd15c45dc3c87a435b05e9b190129494
NyctibiusVII <matheus.dev.07@gmail.com>
2022-07-07 10:35:06 -0300

    ✨ feat: Header component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,22 @@
+/* Import ---------------------------------------------------------------------- */ // - x70
+
 import type { NextPage } from 'next'
+
+import { Header } from '../components/Header'
 
 import Head from 'next/head'
 
 import styles from '../styles/pages/home.module.scss'
 
+/* ---------------------------------------------------------------------- */
+
 const Home: NextPage = () => {
     return (
         <>
-            <Head><title>Create Next App</title></Head>
+            <Head><title>Portfolio | Home</title></Head>
 
             <div className={styles.container}>
-                <h1>Hello World!</h1>
+                <Header />
 
                 <button className={`${styles.abc} line`}>B U T T O N</button>
             </div>
